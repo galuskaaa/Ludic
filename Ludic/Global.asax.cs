25795a183573e21336720d0b9e02b0e88f54e6f4
Ban Istvan <ritamaxim@gmail.com>
2018-07-12 21:39:53 +0300

    -

--- Ludic/Global.asax.cs
+++ Ludic/Global.asax.cs
@@ -14,7 +14,7 @@ namespace Ludic
     {
 		protected void Application_Start()
 		{
-			 AreaRegistration.RegisterAllAreas();
+			AreaRegistration.RegisterAllAreas();
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
