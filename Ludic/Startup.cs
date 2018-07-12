3a3c60b8aa60e3e4bba6e814a704f9ce13607733
Ban Istvan <ritamaxim@gmail.com>
2018-07-12 13:54:22 +0300

    Add project files.

---
+++ Ludic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ludic.Startup))]
+namespace Ludic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
